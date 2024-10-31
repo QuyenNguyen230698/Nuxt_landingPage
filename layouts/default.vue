01af154a6519b415f7b6e2c9dea2918a09b7de1a
Quyen <at128ve@gmail.com>
2024-10-31 15:24:03 +0700

    refactor code

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,21 +8,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, watch } from 'vue';
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-const showHeader = ref(false);
-const scrollY = ref(0);
-
-const updateScroll = () => {
-  scrollY.value = window.scrollY;
-  if (scrollY.value > 50) {
-    showHeader.value = true;
-  } else {
-    showHeader.value = false;
-  }
-};
 
 onMounted(() => {
   AOS.init({
@@ -34,16 +21,9 @@ onMounted(() => {
     mirror: true,
     anchorPlacement: "top-bottom",
   });
-  window.addEventListener("scroll", updateScroll);
 });
 
 onUnmounted(() => {
-  window.removeEventListener("scroll", updateScroll);
 });
 
-watch(scrollY, (newVal) => {
-  if (newVal > 50) {
-    showHeader.value = true;
-  }
-});
 </script>
